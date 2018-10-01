6d8ef8c63307abd91666aeb0d335536c08d75986
Albert Apsit <albert.apsit@gmail.com>
2018-10-01 21:46:19 +0300

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+
+//smooth scrolling by anchor
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
